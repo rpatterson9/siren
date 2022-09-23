666e23291ecd6d3aba736ce10bca3bc281e2e16d
Robert Patterson <robpattersonx@gmail.com>
2022-09-22 17:35:58 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('node:http');
+const dgram = require('node:dgram');
+const { Buffer } = require('node:buffer');
+
+const express = require('express')
+const app = express()
+const port = 3000
+const message = Buffer.from('1234567890123456789');
+const client = dgram.createSocket('udp4');
+
+
+
+
+
+function send() {
+    const message = Buffer.from('1234567890123456789');
+    const client = dgram.createSocket('udp4');
+    client.send(message, 1234, '192.168.1.93', (err) => {
+      client.close();
+    });
+    }
+
+    app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+    res.render('index.html')
+})
+app.get('/siren', (req, res) => {
+    res.send('<h1> WRONG! </h1>')
+
+
+  })
+  app.post('/send', (req, res) => {
+console.log("send")
+send()
+  })
+  app.get('/send', (req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ udp.js
@@ -0,0 +1,10 @@
+
+
+function send() {
+  const message = Buffer.from('1234567890123456789');
+  const client = dgram.createSocket('udp4');
+  client.send(message, 1234, '192.168.1.93', (err) => {
+    client.close();
+  });
+  }
+  exports.send = send;
